077fa573f6f5e8f867a3dc70a2188f2fa073d8d3
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-12 16:43:33 +0100

    Add full-screen map page with Mapbox integration and styling

--- src/app/pages/full-screen-map-page/full-screen-map-page.component.ts
+++ src/app/pages/full-screen-map-page/full-screen-map-page.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { environment } from '@environments/environment';
+import mapboxgl from 'mapbox-gl';
 
+mapboxgl.accessToken = environment.mapBoxApiKey;
 @Component({
   selector: 'app-full-screen-map-page',
   imports: [],
   templateUrl: './full-screen-map-page.component.html',
+  styleUrl: './full-screen-map-page.component.css',
 })
-export class FullScreenMapPageComponent { }
+export class FullScreenMapPageComponent implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+
+  async ngAfterViewInit(): Promise<void> {
+    if (!this.divElement()?.nativeElement) return;
+    const element = this.divElement()!.nativeElement;
+    const map = new mapboxgl.Map({
+      container: element,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }
+}
