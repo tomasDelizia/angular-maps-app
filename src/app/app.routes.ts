f0446f43df182d519f1507a487a053d1f68370bf
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-12 15:52:53 +0100

    Add routing and components for full-screen map, markers, and houses pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullScreenMapPageComponent } from './pages/full-screen-map-page/full-screen-map-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { HousesPageComponent } from './pages/houses-page/houses-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    component: FullScreenMapPageComponent,
+    title: 'Full Screen Map',
+  },
+  {
+    path: 'markers',
+    component: MarkersPageComponent,
+    title: 'Markers',
+  },
+  {
+    path: 'houses',
+    component: HousesPageComponent,
+    title: 'Houses'
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen',
+  },
+];
---
+++ src/app/pages/full-screen-map-page/full-screen-map-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-full-screen-map-page',
+  imports: [],
+  templateUrl: './full-screen-map-page.component.html',
+})
+export class FullScreenMapPageComponent { }
