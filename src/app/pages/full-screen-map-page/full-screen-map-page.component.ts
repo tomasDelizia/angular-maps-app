085d3d352026f0085c895b91ae2bdeb72d3060c2
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-13 08:48:15 +0100

    Add zoom controls to full-screen map page with reactive zoom functionality

--- src/app/pages/full-screen-map-page/full-screen-map-page.component.ts
+++ src/app/pages/full-screen-map-page/full-screen-map-page.component.ts
@@ -1,16 +1,33 @@
-import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  effect,
+  ElementRef,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { environment } from '@environments/environment';
 import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = environment.mapBoxApiKey;
 @Component({
   selector: 'app-full-screen-map-page',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './full-screen-map-page.component.html',
   styleUrl: './full-screen-map-page.component.css',
 })
 export class FullScreenMapPageComponent implements AfterViewInit {
+  zoom = signal(14);
+
   divElement = viewChild<ElementRef>('map');
+
+  map = signal<mapboxgl.Map | null>(null);
+
+  zoomEffect = effect(() => {
+    if (!this.map()) return;
+    this.map()?.setZoom(this.zoom());
+  });
 
   async ngAfterViewInit(): Promise<void> {
     if (!this.divElement()?.nativeElement) return;
@@ -19,7 +36,17 @@ export class FullScreenMapPageComponent implements AfterViewInit {
       container: element,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [-74.5, 40],
-      zoom: 9,
+      zoom: this.zoom(),
     });
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: mapboxgl.Map) {
+    // After zoom effect
+    map.on('zoomend', (event) => {
+      const newZoom = event.target.getZoom();
+      this.zoom.set(newZoom);
+    });
+    this.map.set(map);
   }
 }
