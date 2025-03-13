cf2815e3ea0eb296e255eb4f90a3e2eb30e72cb1
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-13 11:31:39 +0100

    Add marker creation on map click with random colors and unique IDs

--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -7,54 +7,61 @@ import {
 } from '@angular/core';
 import { environment } from '@environments/environment';
 import mapboxgl from 'mapbox-gl';
+import { v4 as uuidv4 } from 'uuid';
+import { getRandomHexColor } from 'src/app/utils/hex-color-generator';
 
 mapboxgl.accessToken = environment.mapBoxApiKey;
+
+interface Marker {
+  id: string;
+  mapboxMarker: mapboxgl.Marker;
+}
+
 @Component({
   selector: 'app-markers-page',
   imports: [],
   templateUrl: './markers-page.component.html',
 })
 export class MarkersPageComponent implements AfterViewInit {
-  zoom = signal(12);
-  coordinates = signal({ lng: -64.1971129, lat: -31.4136803 });
-
   divElement = viewChild<ElementRef>('map');
 
   map = signal<mapboxgl.Map | null>(null);
 
+  markers = signal<Marker[]>([]);
+
   async ngAfterViewInit(): Promise<void> {
     if (!this.divElement()?.nativeElement) return;
     const element = this.divElement()!.nativeElement;
-    const { lng, lat } = this.coordinates();
     const map = new mapboxgl.Map({
       container: element,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: this.zoom(),
+      center: [-64.1971129, -31.4136803],
+      zoom: 12,
     });
-
-    const marker = new mapboxgl.Marker({
-      draggable: false,
-      color: 'red',
-    })
-      .setLngLat(this.coordinates())
-      .addTo(map);
     this.mapListeners(map);
   }
 
   mapListeners(map: mapboxgl.Map) {
-    // After zoom effect
-    map.on('zoomend', (event) => {
-      const newZoom = event.target.getZoom();
-      this.zoom.set(newZoom);
+    map.on('click', (event) => {
+      this.mapClick(event);
     });
-    map.on('moveend', (event) => {
-      const center = map.getCenter();
-      this.coordinates.set(center);
-    });
-    map.addControl(new mapboxgl.FullscreenControl());
-    map.addControl(new mapboxgl.NavigationControl());
-    map.addControl(new mapboxgl.ScaleControl());
     this.map.set(map);
+  }
+
+  private mapClick(event: mapboxgl.MapMouseEvent) {
+    if (!this.map()) return;
+    const mapboxMarker = new mapboxgl.Marker({
+      draggable: false,
+      color: getRandomHexColor(),
+    })
+      .setLngLat(event.lngLat)
+      .addTo(this.map()!);
+
+    const newMarker: Marker = {
+      id: uuidv4(),
+      mapboxMarker,
+    };
+    this.markers.set([newMarker, ...this.markers()]);
+    console.log(this.markers());
   }
 }
